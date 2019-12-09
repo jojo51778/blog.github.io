2db767401b4277743253a6a2576848cf5d27cd24
jojo51778 <623025156@qq.com>
2019-12-09 18:34:47 +0800

    🍻 add eventloop

--- examples/promise/promise.js
+++ examples/promise/promise.js
@@ -160,14 +160,3 @@ Promise.deferred = Promise.defer = function() {
   })
   return dfd
 }
-
-var start = +new Date()
-function foo() {
-  setTimeout(function() {
-    console.log('setTimeout')
-    if((+new Date) - start < 1000) {
-      foo()
-    }
-  })
-}
-foo()
