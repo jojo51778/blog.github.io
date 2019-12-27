47cddad72ade3a578c0345def19d7c6d771e4470
jojo51778 <623025156@qq.com>
2019-12-27 19:02:46 +0800

    🤔 add class

---
+++ examples/es6/class.js
@@ -0,0 +1,120 @@
+// class变量会提升，但不会初始化赋值。Foo进入暂时性死区，类似于let，const变量
+const bar = new Bar(); // it's ok
+function Bar() {
+  this.bar = 42;
+}
+
+const foo = new Foo(); // ReferenceError: Foo is not defined
+class Foo {
+  constructor() {
+    this.foo = 42;
+  }
+}
+// class声明内部采用严格模式
+// 引用一个未声明的变量
+function Bar() {
+  baz = 42; // it's ok
+}
+const bar = new Bar();
+
+class Foo {
+  constructor() {
+    fol = 42; // ReferenceError: fol is not defined
+  }
+}
+const foo = new Foo();
+// class所以方法都不可以枚举
+// 引用一个未声明的变量
+function Bar() {
+  this.bar = 42;
+}
+Bar.answer = function() {
+  return 42;
+};
+Bar.prototype.print = function() {
+  console.log(this.bar);
+};
+const barKeys = Object.keys(Bar); // ['answer']
+const barProtoKeys = Object.keys(Bar.prototype); // ['print']
+
+class Foo {
+  constructor() {
+    this.foo = 42;
+  }
+  static answer() {
+    return 42;
+  }
+  print() {
+    console.log(this.foo);
+  }
+}
+const fooKeys = Object.keys(Foo); // []
+const fooProtoKeys = Object.keys(Foo.prototype); // []
+// class的所有方法都没有原型对象prototype，所以没有构造器，不能用new来使用
+function Bar() {
+  this.bar = 42;
+}
+Bar.prototype.print = function() {
+  console.log(this.bar);
+};
+
+const bar = new Bar();
+const barPrint = new bar.print(); // it's ok
+
+class Foo {
+  constructor() {
+    this.foo = 42;
+  }
+  print() {
+    console.log(this.foo);
+  }
+}
+const foo = new Foo();
+const fooPrint = new foo.print(); // TypeError: foo.print is not a constructor
+
+// 必须要用new调用class
+function Bar() {
+  this.bar = 42;
+}
+const bar = Bar(); // it's ok
+
+class Foo {
+  constructor() {
+    this.foo = 42;
+  }
+}
+const foo = Foo(); // TypeError: Class constructor Foo cannot be invoked without 'new'
+// class内部无法重新类名
+function Bar() {
+  Bar = "Baz"; // it's ok
+  this.bar = 42;
+}
+const bar = new Bar();
+// Bar: 'Baz'
+// bar: Bar {bar: 42}
+
+class Foo {
+  constructor() {
+    this.foo = 42;
+    Foo = "Fol"; // TypeError: Assignment to constant variable
+  }
+}
+const foo = new Foo();
+Foo = "Fol"; // it's ok
+// 继承的差异
+class Super {}
+class Sub extends Super {}
+
+const sub = new Sub();
+
+Sub.__proto__ === Super;
+
+function Super() {}
+function Sub() {}
+
+Sub.prototype = new Super();
+Sub.prototype.constructor = Sub;
+
+var sub = new Sub();
+
+Sub.__proto__ === Function.prototype;
